8364b72233b3e836d1c090843ec94ce15f71fefe
Armin <armin2003sheibak@gmail.com>
2025-04-04 19:33:33 +0330

    add random pick

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -1,42 +1,49 @@
-import { Drawer, Box, HStack, VStack, CloseButton, Image } from '@chakra-ui/react'
-import { RiMenu4Fill } from 'react-icons/ri'
-import { Link } from 'react-router-dom'
-import logo from "../assets/logo.png"
+import {
+  Drawer,
+  Box,
+  HStack,
+  VStack,
+  CloseButton,
+  Image,
+} from "@chakra-ui/react";
+import { RiMenu4Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const NavbarMenu = () => {
   return (
-    <Drawer.Root >
-        <Drawer.Trigger asChild>
-          <Box hideFrom={"md"} marginTop={4}>
-            <RiMenu4Fill size={36} color={"#7a7a7a"} />
-          </Box>
-        </Drawer.Trigger>
-        <Drawer.Backdrop />
-        <Drawer.Positioner >
-          <Drawer.Content>
-            <Drawer.Header>
-              <HStack>
-                <Link to={"/"}>
-                  <Image src={logo} height={"40px"} />
-                </Link>
-                <Drawer.Title marginTop={1} >Mr. Chef</Drawer.Title>
-              </HStack>
-            </Drawer.Header>
-            <Drawer.Body >
-              <VStack gap={5} marginTop={3}>
-                <Link to={"/recipes"}>Recipes</Link>
-                <Link to={"/"}>Trending</Link>
-                <Link to={"/favorites"}>Favorites</Link>
-                <Link to={"https://github.com/arminsheibak"}>About Us</Link>
-              </VStack>
-            </Drawer.Body>
-            <Drawer.CloseTrigger asChild>
-              <CloseButton size="sm" />
-            </Drawer.CloseTrigger>
-          </Drawer.Content>
-        </Drawer.Positioner>
-      </Drawer.Root>
-  )
-}
+    <Drawer.Root initialFocusEl={() => null}>
+      <Drawer.Trigger asChild>
+        <Box hideFrom={"md"} marginTop={4}>
+          <RiMenu4Fill size={36} color={"#7a7a7a"} />
+        </Box>
+      </Drawer.Trigger>
+      <Drawer.Backdrop />
+      <Drawer.Positioner>
+        <Drawer.Content>
+          <Drawer.Header>
+            <HStack>
+              <Link to={"/"}>
+                <Image src={logo} height={"40px"} />
+              </Link>
+              <Drawer.Title marginTop={1}>Mr. Chef</Drawer.Title>
+            </HStack>
+          </Drawer.Header>
+          <Drawer.Body>
+            <VStack gap={5} marginTop={3}>
+              <Link to={"/recipes"}>Recipes</Link>
+              <Link to={"/"}>Trending</Link>
+              <Link to={"/favorites"}>Favorites</Link>
+              <Link to={"https://github.com/arminsheibak"}>About Us</Link>
+            </VStack>
+          </Drawer.Body>
+          <Drawer.CloseTrigger asChild>
+            <CloseButton size="sm" />
+          </Drawer.CloseTrigger>
+        </Drawer.Content>
+      </Drawer.Positioner>
+    </Drawer.Root>
+  );
+};
 
-export default NavbarMenu
+export default NavbarMenu;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
-import { Box, Heading, HStack, Image, VStack } from "@chakra-ui/react";
+import { Box, Card, Heading, HStack, Image, VStack } from "@chakra-ui/react";
 import bg from "../assets/bg.jpg";
 import bgMobile from "../assets/bg-mobile.jpg";
 import SearchInput from "../components/SearchInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import NavbarMenu from "../components/NavbarMenu";
+import rand from "../assets/random.jpg";
+import colorPalette from "../theme";
+import apiClient from "../services/apiClient";
+import { meal } from "../entities/meal";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [random, setRandom] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    apiClient
+    .get<{ meals: meal[] }>("random.php")
+    .then((res) => setRandom(res.data.meals[0].idMeal))
+  }, [])
   return (
     <>
       <HStack
@@ -29,7 +42,7 @@ const Home = () => {
           <NavbarMenu />
         </Box>
       </HStack>
-      <Box paddingLeft={{base: 0, md: "60px"}} >
+      <Box paddingLeft={{ base: 0, md: "60px" }}>
         <Image
           hideBelow={"lg"}
           src={bg}
@@ -50,7 +63,7 @@ const Home = () => {
           height={"100vh"}
           objectFit={"cover"}
         />
-        <VStack align={{base: "center", md: "flex-start"}} >
+        <VStack align={{ base: "center", md: "flex-start" }}>
           <Heading
             fontWeight={600}
             fontSize={{ base: "24px", md: "32px" }}
@@ -61,6 +74,28 @@ const Home = () => {
           <Box width={{ base: "85%", md: "60%", lg: "46%" }}>
             <SearchInput />
           </Box>
+          <Card.Root
+            borderRadius={10}
+            shadow={"sm"}
+            overflow="hidden"
+            _hover={{
+              transform: "scale(1.03)",
+              transition: "transform 0.15s ease-in",
+            }}
+            height={"245px"}
+            width={"300px"}
+            borderColor={colorPalette.primary[300]}
+            marginY={{ base: 10, md: 5 }}
+            opacity={"0.9"}
+            onClick={() => {
+              navigate(`/recipes/${random}`)
+            }}
+          >
+            <Image src={rand} maxHeight={"80%"} />
+            <Heading as={"h3"} paddingX={3} paddingY={2}>
+              Random Recipe
+            </Heading>
+          </Card.Root>
         </VStack>
       </Box>
     </>
